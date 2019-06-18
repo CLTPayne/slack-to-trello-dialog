828bf4bea43f6b38ccfcccf2624349c98abb84f3
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-06-18 15:52:01 -0400

    Implement request body parsing
    * Handle form url-encoded
    * Handle json

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-require('dotenv').config()
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+const qs = require('qs');
+const express = require('express');
+const app = express();
+
+// Parse urlencoded bodies with the qs library
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
