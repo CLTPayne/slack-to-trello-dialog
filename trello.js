f3bf742ddec0b29aa2315fe14a1d2657e5695e01
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-06-19 19:50:32 -0400

    Fix card creation and confirmation from trello

--- trello.js
+++ trello.js
@@ -32,11 +32,17 @@ const trelloList = process.env.TRELLO_LIST_ID;
 // dialog with blockit
 
 const sendConfirmation = card => {
+    // TODO - use block kit for this confirmation message
+    // Make it appear in thread. 
+    // Messages come from slack bot / how best to have a confirmation???
     try {
         const result = axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
             token: process.env.SLACK_ACCESS_TOKEN,
             channel: card.userId,
             text: 'Bug ticket created in Trello',
+            as_user: false,
+            username: 'Scout Bugs',
+            icon_emoji: ':bug:',
             attachments: JSON.stringify([
                 {
                     title: `Trello card created by ${card.userRealName}`,
@@ -73,7 +79,7 @@ const createCard = async (userId, submission) => {
         try {
             const userInfo = await users.find(userId) // logic to access the users userId 
             debug(`Find user: ${userId}`)
-            console.log(userName.data.user)
+            console.log('👩🏻', userInfo.data.user.profile.real_name_normalized)
             return userInfo.data.user.profile.real_name_normalized
         } catch (error) {
             console.log(error)
@@ -88,12 +94,16 @@ const createCard = async (userId, submission) => {
         card.userRealName = userName;
         card.title = submission.title;
         card.summary = submission.summary;
-        const result = await trelloApi.post('/cards', qs.stringify({
+        const result = await axios.post('https://api.trello.com/1/cards', {
             idList: trelloList, // id of the backlog list in the test board
             name: submission.title,
             desc: `${submission.summary}\n\n---\n Submitted by ${card.userRealName}`,
-        }))
+            key: process.env.TRELLO_KEY,
+            token: process.env.TRELLO_ACCESS_TOKEN
+        })
         card.shortUrl = result.data.shortUrl;
+        // TODO - Received a confirmation but it was from 'Slackbot and not direct in the thread
+        // This needs to be better UX
         sendConfirmation(card)
         return card
     } catch (error) {
