f1e10d40f7ebfa2420db7c3aaf6915c49f5a80a6
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-06-20 16:12:33 -0400

    Refactor user module to user es6

--- users.js
+++ users.js
@@ -1,12 +1,9 @@
-const qs = require('querystring');
-const axios = require('axios');
+import axios from 'axios';
 
-const find = async slackUserId => {
+export const find = slackUserId => {
     const body = {
         token: process.env.SLACK_ACCESS_TOKEN,
         user: slackUserId
     };
-    return axios.post('https://slack.com/api/users.info', qs.stringify(body))
-}
- 
-module.exports = { find }
+    return axios.post('https://slack.com/api/users.info', body)
+};
